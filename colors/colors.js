281513255890b856609095395cd98d32c9bc9a38
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2021-02-16 23:50:24 +0100

    feat: filters and color utils

---
+++ colors/colors.js
@@ -0,0 +1,20 @@
+const palette = require('google-palette')
+
+export default (colorscheme, nodes) => {
+  const typeMax = {
+    qualitative: 8,
+    diverging: 11,
+    sequential: 9
+  }
+  let offset = 0
+  if (colorscheme.subset && colorscheme.subset !== 'all') offset = typeMax[colorscheme.type] - nodes.length
+  let colors = palette('cb-' + colorscheme.name, Math.min(nodes.length + offset, typeMax[colorscheme.type]))
+  if (colors) colors = colors.map(c => '#' + c)
+  else colors = ['#999999']
+  if (offset > 0) {
+    if (colorscheme.subset === 'light') colors = colors.slice(0, colors.length - offset)
+    if (colorscheme.subset === 'dark') colors = colors.slice(offset)
+  }
+  if (colorscheme.reverse) colors.reverse()
+  return Object.assign({}, ...nodes.map((n, i) => ({ [n]: colors[i % colors.length] })))
+}
