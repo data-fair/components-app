25c42bb7b14bde0850c4aec1d12fa6f72b0dd763
Alban Mouton <alban.mouton@gmail.com>
2021-08-25 21:43:01 +0200

    fix: escape elasticsearch filter values

--- filters/filters.js
+++ filters/filters.js
@@ -1,21 +1,55 @@
+// small tools to transform objects containing simple data filter into elasticsearch querystring syntax
+
 export function filter2qs(filter) {
+  const key = escape(filter.field.key)
+
   if (!filter.type || filter.type === 'in') {
     if ([null, undefined, ''].includes(filter.values)) return null
     if (Array.isArray(filter.values) && filter.values.length === 0) return null
-    return filter.values.map(v => `${filter.field.key}:"${v}"`).join(' OR ')
+    return filter.values.map(v => `${key}:"${escape(v)}"`).join(' OR ')
   } else if (filter.type === 'out') {
     if ([null, undefined, ''].includes(filter.values)) return null
     if (Array.isArray(filter.values) && filter.values.length === 0) return null
-    return filter.values.map(v => `!(${filter.field.key}:"${v}")`).join(' AND ')
+    return filter.values.map(v => `!(${key}:"${escape(v)})"`).join(' AND ')
   } else if (filter.type === 'interval') {
     if (!filter.minValue || !filter.maxValue) return null
-    return `${filter.field.key}:[${filter.minValue} TO ${filter.maxValue}]`
+    return `${escape(filter.field.key)}:[${filter.minValue} TO ${filter.maxValue}]`
   }
 }
 
-export function filters2qs(filters) {
-  return (filters|| [])
+export function filters2qs(filters = []) {
+  return filters
+    .filter(f => !!f)
     .map(filter2qs)
     .filter(f => !!f)
     .map(f => `(${f})`).join(' AND ')
 }
+
+// cf https://github.com/joeybaker/lucene-escape-query/blob/master/index.js
+export function escape (val) {
+  if (typeof val !== 'string') return val
+  return [].map.call(val, (char) => {
+    if (char === '+' ||
+      char === '-' ||
+      char === '&' ||
+      char === '|' ||
+      char === '!' ||
+      char === '(' ||
+      char === ')' ||
+      char === '{' ||
+      char === '}' ||
+      char === '[' ||
+      char === ']' ||
+      char === '^' ||
+      char === '"' ||
+      char === '~' ||
+      char === '*' ||
+      char === '?' ||
+      char === ':' ||
+      char === '\\' ||
+      char === '/'
+    ) return '\\' + char
+    else return char
+  }).join('')
+}
+
