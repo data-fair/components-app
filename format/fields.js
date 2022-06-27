62fd6afc625f32fc9ed084c9ed61a066bec1e3eb
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2022-06-27 14:44:57 +0200

    fix: number & date formatting

--- format/fields.js
+++ format/fields.js
@@ -3,7 +3,7 @@ import dayjs from 'dayjs'
 export function formatField(item, field) {
     if (!item[field.key]) return ''
     if (field['x-labels']) return field['x-labels'][item[field.key]]
-    if (field.type === 'number' || field.type === 'integer') return item[field.key].toLocaleString()
-    if (field['x-refersTo'] === 'http://schema.org/Date') return dayjs(item[field.key]).format('DD/MM/YYYY')
+    if (field.type === 'number' || field.type === 'integer') return item[field.key].toLocaleString('fr')
+    if (field['x-refersTo'] === 'http://schema.org/Date' || field['x-refersTo'] === 'https://schema.org/startDate' || field['x-refersTo'] === 'https://schema.org/endDate') return dayjs(item[field.key]).format('DD/MM/YYYY')
     return item[field.key]
 }
