4bbb567ae277c14b75099799eb8df3f380972432
Alban Mouton <alban.mouton@gmail.com>
2022-11-18 11:04:07 +0100

    fix: fix: missing escaping of interval values in QS filter + other improvements to filters utils

--- filters/filters.js
+++ filters/filters.js
@@ -1,6 +1,6 @@
 // small tools to transform objects containing simple data filter into elasticsearch querystring syntax
 
-export function filter2qs(filter) {
+export function filter2qs (filter, locale = 'fr') {
   if (typeof filter === 'string') return filter
 
   const key = escape(filter.field.key)
@@ -17,6 +17,15 @@ export function filter2qs(filter) {
     const min = ![null, undefined, ''].includes(filter.minValue) ? escape(filter.minValue) : '*'
     const max = ![null, undefined, ''].includes(filter.maxValue) ? escape(filter.maxValue) : '*'
     return `${key}:[${min} TO ${max}]`
+  } else if (filter.type === 'starts') {
+    if ([null, undefined, ''].includes(filter.value)) return null
+    if (filter.value.includes(',')) {
+      throw new Error({
+        fr: 'vous ne pouvez pas appliquer un filtre "commence par" contenant une virgule',
+        en: 'You cannot use a filter "startsWith" containing a comma'
+      }[locale])
+    }
+    return `${key}:${escape(filter.value)}*`
   }
 }
 
@@ -49,6 +58,7 @@ export function escape (val) {
       char === '*' ||
       char === '?' ||
       char === ':' ||
+      char === ' ' ||
       char === '\\' ||
       char === '/'
     ) return '\\' + char
