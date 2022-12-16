6bc0f2f7e3ab0aa7a7c3dcfc527740a152b5758c
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2022-12-16 15:00:13 +0100

    fix: x-labels display when only key is set

--- format/fields.js
+++ format/fields.js
@@ -2,7 +2,7 @@ import dayjs from 'dayjs'
 
 export function formatField(item, field) {
     if (!item[field.key]) return ''
-    if (field['x-labels']) return field['x-labels'][item[field.key]]
+    if (field['x-labels'] && field['x-labels'][item[field.key]]) return field['x-labels'][item[field.key]]
     if (field.type === 'number' || field.type === 'integer') return item[field.key].toLocaleString('fr')
     if (field['x-refersTo'] === 'http://schema.org/Date' || field['x-refersTo'] === 'https://schema.org/startDate' || field['x-refersTo'] === 'https://schema.org/endDate') return dayjs(item[field.key]).format('DD/MM/YYYY')
     return item[field.key]
