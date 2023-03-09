ab86c8c1e1ae6176ef8de41d9a0484479ca3072f
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2023-03-09 10:07:12 +0100

    fix: date formating

--- format/fields.js
+++ format/fields.js
@@ -4,6 +4,14 @@ export function formatField(item, field) {
     if (!item[field.key]) return ''
     if (field['x-labels'] && field['x-labels'][item[field.key]]) return field['x-labels'][item[field.key]]
     if (field.type === 'number' || field.type === 'integer') return item[field.key].toLocaleString('fr')
-    if (field['x-refersTo'] === 'http://schema.org/Date' || field['x-refersTo'] === 'https://schema.org/startDate' || field['x-refersTo'] === 'https://schema.org/endDate') return dayjs(item[field.key]).format('DD/MM/YYYY')
+    if (
+        field['x-refersTo'] === 'http://schema.org/Date' ||
+        field['x-refersTo'] === 'https://schema.org/startDate' ||
+        field['x-refersTo'] === 'https://schema.org/endDate' ||
+        field['x-refersTo'] === 'http://schema.org/dateCreated'
+    ) {
+        if (field.format === 'date-time') return dayjs(item[field.key]).format('DD/MM/YYYY, HH[h]mm')
+        else return dayjs(item[field.key]).format('DD/MM/YYYY')
+    }
     return item[field.key]
 }
